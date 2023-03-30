025588a79b0378a6cedc1126572b6f214b06de5e
hernnnnnn <1211202010@student.mmu.edu.my>
2023-03-30 23:09:30 +0800

    yy

--- registrationlogin.php
+++ registrationlogin.php
@@ -5,32 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration / Login</title>
-    <link rel="stylesheet" href="https://cdn.lineicons.com/4.0/lineicons.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
     <link rel="stylesheet" href="css/registrationlogin.css">
 </head>
+
 <body>
     <div class="container" id="container">
         <div class="form-container register-container">
         <form action="#">
-            <h1>Register</h1>
-            <input type="text" placeholder="Name">
-            <input type="email" placeholder="Email">
-            <input type="password" placeholder="Password">
-            <button>Register</button>
-            <span>or use your account</span>
-            <div class="social-container">
-            <a href="#" class="social"><i class="lni lni-facebook-fill"></i></a>
-            <a href="#" class="social"><i class="lni lni-google"></i></a>
-            <a href="#" class="social"><i class="lni lni-linkedin-original"></i></a>
+            <h1>User Registration</h1>
+            <input type="text" id="name" placeholder="Name">
+            <input type="email" id="email" placeholder="Email">
+            <div class="password">
+                <input type="password" id="passwd" placeholder="Password">
+                <span id="togglebtn"></span>
+                <div class="validation">
+                    <ul>
+                        <li id="length">Minimum 8 character</li>
+                        <li id="number">At least one number</li>
+                        <li id="lower">At least one lowercase character</li>
+                        <li id="upper">At least one uppercase character</li>
+                        <li id="special">At least one special character</li>
+                    </ul>
+                </div>
             </div>
+        
+            <button>Register</button>
         </form>
         </div>    
         
         <div class="form-container login-container">
         <form action="#">
-            <h1>Login</h1>
-            <input type="email" placeholder="Email">
-            <input type="password" placeholder="Password">
+            <h1>User Login</h1>
+            <input type="email" id="email" placeholder="Email">
+            <input type="password" id="passwd" placeholder="Password">
+
             <div class="content">
             <div class="checkbox">
                 <input type="checkbox" name="checkbox" id="checkbox">
@@ -41,12 +50,6 @@
             </div>
             </div>
             <button>Login</button>
-            <span>or use your account</span>
-            <div class="social-container">
-            <a href="#" class="social"><i class="lni lni-facebook-fill"></i></a>
-            <a href="#" class="social"><i class="lni lni-google"></i></a>
-            <a href="#" class="social"><i class="lni lni-linkedin-original"></i></a>
-            </div>
         </form>
         </div>
 
@@ -56,7 +59,7 @@
                     <h1 class="title">Hello <br> Welcome </h1>
                     <p>if you don't have an account, login here and have fun</p>
                     <button class="ghost" id="login">Login
-                        <i class="lni lni-arrow-left login"></i>
+                        <i class="fa-regular fa-arrow-left"></i>
                     </button>
                 </div>
 
@@ -64,7 +67,7 @@
                     <h1 class="title">Start your <br> journey now </h1>
                     <p>if you don't have an account, join us now</p>
                     <button class="ghost" id="register">Register
-                        <i class="lni lni-arrow-right register"></i>
+                        <i class="fa-regular fa-arrow-right"></i>
                     </button>
                 </div>
             </div>
