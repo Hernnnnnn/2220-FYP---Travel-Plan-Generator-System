99ca70d50d71a59737ae856c214029f585524910
Teng <dehui7051@gmail.com>
2023-04-05 15:42:53 +0800

    change

--- adminprofile.php
+++ adminprofile.php
@@ -1,8 +1,4 @@
-<?php
-    include 'adminnavbar.php';
 
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,11 +6,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin || Profile</title>
+    <?php
+    include 'adminnavbar.php';
+    ?>
+    <link rel="stylesheet" href="css/adminprofile.css">
 </head>
-<body>
+<body style="background-image: url(images/image.gif);background-size: cover; ">
+                <div class="container2">
+                        <div class="profile">
+                            <a href=""><img src="images/setting.png" class="setting-icon" alt=""></a>
+                            <img src="images/profile.jpg" class="profile-pic" alt="">
+                            <h3>Teng De Hui</h3>
+                            <p>Admin of Travel Generators</p>
+
+                        </div>
+                    </div>
+                
+                
+
+        </div>
     </div>
-    </div>
+</div>
+</div>
 
 </body>
 </html>
