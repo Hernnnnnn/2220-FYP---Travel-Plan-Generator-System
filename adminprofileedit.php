ee3734c8b6d94a9df5307c63ed638c7b35ed53b1
Teng <dehui7051@gmail.com>
2023-04-10 14:34:20 +0800

    change

--- adminprofileedit.php
+++ adminprofileedit.php
@@ -7,13 +7,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin || Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <?php
     include 'adminnavbar.php';
     $email = $_GET['email'];
     $sql = "SELECT * From `admin` where email = '$email'";
 	$r = mysqli_query($conn,$sql);
 	$re = mysqli_fetch_assoc($r);
+    $msg='';
     ?>
+    <?php
+
+if(isset($_POST['submit']))
+{
+    $password = mysqli_real_escape_string($conn,$_POST['password']);
+    if($password != ""  && $password != $re['password'] )
+    {
+        mysqli_query($conn,"UPDATE `admin` set password = '$password' Where email = '$email'");
+        $msg="<div class='success'><i class='fa fa-check-circle-o'></i>&nbsp;Change successful</div>";
+    }
+    else
+    {
+        if($password == $re['password'])
+        {
+            $msg="<div class='error'><i class='fa fa-exclamation-circle '></i>&nbsp;Your new password cannot be the same as your current password</div>";
+        }
+        else if($password == "")
+        {
+            $msg="<div class='error'><i class='fa fa-exclamation-circle '></i>&nbsp;Please fill in your password</div>";
+        }
+    }
+}
+?>
     
 </head>
 <style>
@@ -121,6 +147,52 @@ body
     /* text-align: center; */
     margin-left: 320px;
 }
+input[type="submit"]
+{
+    border: none;
+    width: 190px;
+    height: 50px;
+    padding: 5px 20px;
+    margin-top: 20px;
+    /* margin-left: 100px; */
+    background: #4bb6b7;
+    border-radius: 30px;
+    font-size: 20px;
+    color: white;
+    
+}
+input[type="submit"]:hover
+{
+    background-color: #6c757d;
+    color: white;
+}
+.success
+{
+    color: #2d6a4f;
+    background-color: #52b788;
+    /* font-weight: bold; */
+    margin-bottom: 10px;
+    padding: 8px;
+    width: 370px;
+    /* font-size: 10pt; */
+    border-radius: 10px;
+    /* text-transform: uppercase; */
+    text-align: center;
+}
+.error
+{
+    color: #d43838;
+    background-color: #fcd1d1;
+    /* font-weight: bold; */
+    margin-bottom: 10px;
+    padding: 8px;
+    width: 370px;
+    /* font-size: 10pt; */
+    border-radius: 10px;
+    /* text-transform: uppercase; */
+    text-align: center;
+}
+
 </style>
 <body >
             <div class="wrapper">
@@ -131,17 +203,24 @@ body
                     <p>Admin of Travel Plan Generator</p>
                 </div>
                 <div class="right">
-                <a href="adminprofileedit.php?email=<?php echo $re['email'];?>"><img src="images/setting1.png" class="settingimg" ></a>
+                <a href="adminprofile.php?email=<?php echo $re['email'];?>"><img src="images/back.png" class="settingimg" ></a>
                     <div class="info">
-                    
-                        <h3>Information</h3>
+                        <h3>Edit Information</h3>
                         <div class="info_data">
                             <div class="data">
+                    <?php echo $msg;?>
+
                                 <h4>Email</h4>
                                 <p><?php echo $re['email']?></p>
                                 <div class="data">
-                                    <h4>Phone</h4>
-                                    <p>0112112122</p>
+                                    <form action="" method="post">
+                                    <h4>Password</h4>
+                                    <input type="text" name="password">
+                                    <br>
+                                    <input type="submit" value="Submit" name="submit">
+                                    
+                                    </form>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -152,4 +231,6 @@ body
 </div>
 
 </body>
+
+
 </html>
