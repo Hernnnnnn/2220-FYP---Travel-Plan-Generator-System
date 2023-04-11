c8281a87ff181f955104ca44e43437fe1517740f
Teng <dehui7051@gmail.com>
2023-04-11 09:43:31 +0800

    change

--- adminLogin.php
+++ adminLogin.php
@@ -1,3 +1,5 @@
+
+
 <?php
     include "dataconnection.php";
     $msg=" ";
@@ -43,7 +45,10 @@
     <link rel="stylesheet" href="https://cdn.lineicons.com/4.0/lineicons.css"/>
     <link rel="stylesheet" href="css/registrationlogin.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <script>
+        
+        Response.setHeader("Cache-Control")
+    </script>
 
 
     <style>
@@ -125,7 +130,8 @@
         </div>
     </div>
 
-    <script src="registrationlogin.js"></script>
+    <script>
+    </script>
 
 </body>
 </html>
--- adminprofile.php
+++ adminprofile.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin || Profile</title>
+    <link rel="stylesheet" href="css/adminprofile.css">
     <?php
     include 'adminnavbar.php';
     $email = $_GET['email'];
@@ -17,110 +18,13 @@
     
 </head>
 <style>
-    *
-{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    list-style: none;
-    font-family: sans-serif;
-}
-body
-{
+    body
+    {
     background-image: url(images/image.gif);
     background-size: cover; 
     background-repeat: no-repeat;
     background-attachment: fixed;
-}
-.wrapper
-{
-    /* position: relative; */
-    margin: auto;
-    margin-top: 150px;
-    max-width: 650px;
-    height: 350px;
-    display: flex;
-    box-shadow: 0 1px 20px 0 rgba(75, 182, 183,0.9);
-}
-.wrapper .left
-{
-    width: 35%;
-    background: rgba(0,0,0,0.7);
-    padding: 40px 25px;
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
-    text-align: center;
-}
-.wrapper .left img
-{
-    margin-bottom: 10px;
-    border-radius: 5px;
-}
-.wrapper .left h4
-{
-    margin-bottom: 10px;
-    color: #fff;
-
-}
-.wrapper .left p
-{
-    font-size: 15px;
-    color: #fff;
-
-}
-
-.wrapper .right
-{
-    width: 65%;
-    background: rgba(255, 255, 255, 0.7);
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
-    padding: 30px 25px;
-}
-.wrapper .right .info
-{
-    margin-bottom: 25px;
-}
-
-.wrapper .right .info h3
-{
-    margin-bottom: 15px;
-    padding-bottom: 5px;
-    border-bottom:  1px solid black;
-    color: #343c4e;
-    text-transform: uppercase;
-    letter-spacing: 5px;
-    
-}
-.wrapper .right .info_data
-{
-    display: flex;
-    justify-content: space-between;
-}
-.wrapper .right .info_data .data
-{
-    width: 45%;
-}
-.wrapper .right .info_data .data h4
-{
-    color: #353c4e;
-    margin-bottom: 5px;
-}
-.wrapper .right .info_data .data p
-{
-    font-size: 15px;
-    margin-bottom: 10px;
-    color: #6c757d;
-}
-.settingimg
-{
-    width: 20px;
-    display: block;
-    position: absolute;
-    /* right: 2px; */
-    /* text-align: center; */
-    margin-left: 320px;
-}
+    }
 </style>
 <body >
             <div class="wrapper">
