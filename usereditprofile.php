709390c31d08cd71feb696db891907ad10c5953b
hernnnnnn <1211202010@student.mmu.edu.my>
2023-06-06 13:25:52 +0800

    hahahahahaha

--- usereditprofile.php
+++ usereditprofile.php
@@ -2,35 +2,8 @@
 session_start();
 include('dataconnection.php');
 include "usernavbar.php";
-
-if (isset($_POST['update'])) {
-    $prof = $_FILES['image'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone_number'];
-
-    $username = $_SESSION['username'];
-
-    if (!empty($prof['name'])) {
-        $profName = $prof['name'];
-        $profTmpName = $prof['tmp_name'];
-        $profError = $prof['error'];
-
-        $targetPath = "images/" . $profName;
-        move_uploaded_file($profTmpName, $targetPath);
-
-        $updateQuery = "UPDATE login SET image = '$targetPath' WHERE username = '$username'";
-        mysqli_query($conn, $updateQuery);
-    }
-
-    $updateQuery = "UPDATE login SET email = '$email', phone_number = '$phone' WHERE username = '$username'";
-
-    if (mysqli_query($conn, $updateQuery)) {
-        echo "Profile updated successfully.";
-    } else {
-        echo "Error updating profile: " . mysqli_error($conn);
-    }
-}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -38,7 +11,7 @@ if (isset($_POST['update'])) {
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>TPGS || User Profile</title>
+	<title>TPGS || Edit Profile</title>
 </head>
 <style>
 	body{
@@ -49,40 +22,37 @@ if (isset($_POST['update'])) {
     }
 </style>
 <body>
-	<div class="wrapper" style="align-items: center; height: 60%;">
-		<div class="left">
-			<img src="images/<?php echo $result['image']?>" width="100">
-			<h4><?php echo $result['username']?></h4>
-			<p>User of Travel Plan Generator</p>
-
-			<form action=" " method="post" enctype="multipart/form-data">
-				<input type="file" name="image">
-				<input type="submit" name="update" value="Change Picture" class="editprofbtn">
-			</form>
-
-			<form action="userprofile.php" method="post">
-				<input type="email" name="email" value="<?php echo $result['email']?>" required>
-				<input type="text" name="phone_number" value="<?php echo $result['phone_number']?>" required>
-				<input type="submit" name="update" value="Update Profile" class="editprofbtn">
-			</form>
+<body>
+	<div class="editcon">
+		<h1>Edit User Profile</h1>
+		<div class="profile-section">
+			<div class="profile-image">
+				<img id="preview-image" src="images/<?php echo $result['image'] ?>" alt="Profile Image">
+			</div>
+			<div class="profile-details">
+				<form action="userprofile.php?email=<?php echo $email; ?>" method="post" enctype="multipart/form-data">
+					<div class="form-group">
+						<label for="username">Username</label>
+						<input type="text" id="username" name="username" value="<?php echo $result['username'] ?>">
+					</div>
+					<div class="form-group">
+						<label for="contact-number">Contact Number</label>
+						<input type="text" id="contact-number" name="contact_number" value="<?php echo $result['phone_number'] ?>">
+					</div>
+					<div class="form-group">
+						<label for="profile-image">Profile Image</label>
+						<input type="file" id="profile-image" name="profile_image" onchange="previewImage(event)">
+					</div>
+					<div class="form-group">
+						<label for="email">Email</label>
+						<input type="email" id="email" name="email" value="<?php echo $result['email'] ?>">
+					</div>
+					<button type="submit" class="btn">Update Profile</button>
+				</form>
+			</div>
 		</div>
-		
-		<div class="right" style="height: 68%;">
-        <div class="info">
-            <h3>Information</h3>
-            <div class="info_data">
-                <div class="data">
-                    <h4>Email</h4>
-                    <input type="email" name="email" value="<?php echo $result['email']?>" required>
-                </div>
-                <div class="data">
-                    <h4>Phone</h4>
-                    <input type="text" name="phone" value="<?php echo $result['phone_number']?>" required>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+	</div>
 	<?php include "userfooter.php"; ?>
+
 </body>
 </html>
