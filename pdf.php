6b8b31b3e3239427c58628003f49bc4fde34824a
Teng <dehui7051@gmail.com>
2023-06-07 15:58:00 +0800

    hange

--- pdf.php
+++ pdf.php
@@ -1,5 +1,18 @@
 <?php
 include 'dataconnection.php'; 
 
-$res
+$res = mysqli_query($conn,"SELECT * from userfeedback");
+
+if(mysqli_num_rows($res)>0)
+{
+    $html ='<table>';
+        $html.='<tr><td>Day</td><td>Locations</td></tr>';
+
+        while($row = mysqli_fetch_assoc($res))
+        {
+            $htmls.='<tr><td>'.$row['des_day'].'</td><td>'.$row['name'].'</td></tr>';
+        }
+    $html.='</table>';
+
+}
 ?>
