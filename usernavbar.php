98c7de0e1fe610397c4e5ef891254dfff0154293
hernnnnnn <1211202010@student.mmu.edu.my>
2023-06-07 10:38:56 +0800

    sda

--- usernavbar.php
+++ usernavbar.php
@@ -24,7 +24,6 @@ $result = mysqli_fetch_assoc($check);
                     <li><a href="userhomepage.php?email=<?php echo $email; ?>">Home</a></li>
                     <li><a href="usergenerator.php?email=<?php echo $email; ?>">Itinerary</a></li>
                     <li><a href="userhpstate.php?email=<?php echo $email; ?>">State</a></li>
-                    <li><a href="?email=<?php echo $email; ?>">About</a></li>
                 </ul>
             <div class="profile-dropdown">
                 <div class="profile-dropdown-btn" onclick="toggle()">
